c85ef737774d7885d4302563c3a7284dd05fc91f
JOAO H DECASTRO <66505539+JoaoDeCastro@users.noreply.github.com>
2021-03-20 00:52:24 -0400

    adding some plot

--- R code.R
+++ R code.R
@@ -1,5 +1,9 @@
 corona_data = read.csv("C:/Users/joaoh/Downloads/owid-covid-data.csv", header = TRUE, sep=",")
 
+
+
+##################### understanding dataset ######################
+
 #get information from first 6 elements
 head(corona_data)
 #get all names in dataset
@@ -19,15 +23,38 @@ head(corona_data.brazil)
 View(corona_data.brazil)
 plot(corona_data.brazil$new_cases, type = 'o')
 
-View(corona_data)
+
+
 
 #create another data frame
 
 brazil_data <- data.frame(corona_data[9710,6])
 View(brazil_data)
 
+###################### filtering data to get brazil ###############
 
 #filtering data
 #install.packages("dplyr")
 library(dplyr)
 
+
+#using select function to use only wanted variables
+
+new.subset <- corona_data %>% select(location, new_cases, date, new_deaths)
+View(new.subset)
+
+# getting only brazil data
+
+brazil.subset <- corona_data.brazil %>% select(location, new_cases, date, new_deaths)
+View(brazil.subset)
+
+#plot brazil new dataset
+plot(brazil.subset$new_cases, xlab = "Index", ylab = "New Cases", type = 'l')
+
+
+install.packages("ggplot2")
+install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
+
+hist(brazil.subset$new_cases)
