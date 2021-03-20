201ee5af596ac428e53311dcfbed5973a98120c4
JOAO H DECASTRO <66505539+JoaoDeCastro@users.noreply.github.com>
2021-03-20 00:36:44 -0400

    cleaning data

--- R code.R
+++ R code.R
@@ -7,3 +7,27 @@ names(corona_data)
 
 str(corona_data)
 summary(corona_data)
+
+
+#cleaning data to show only Brazil data
+brazil = corona_data[ which(corona_data$location == "Brazil" ),]
+
+#showing only 2021 new cases
+
+corona_data.brazil = corona_data[which(corona_data$location == "Brazil" & corona_data$date >= "2021-01-01"),]
+head(corona_data.brazil)
+View(corona_data.brazil)
+plot(corona_data.brazil$new_cases, type = 'o')
+
+View(corona_data)
+
+#create another data frame
+
+brazil_data <- data.frame(corona_data[9710,6])
+View(brazil_data)
+
+
+#filtering data
+#install.packages("dplyr")
+library(dplyr)
+
